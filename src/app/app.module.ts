7005275c9ca90e1b12540efb5f66803df718291f
Damian Brzoskowski - s18499 <s18499@pjwstk.edu.pl>
2021-06-06 18:24:11 +0200

    Add product and product-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './product/item.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ItemListComponent,
+    ProductListComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
