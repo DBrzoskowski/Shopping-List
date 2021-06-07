54bf23014a77e82cb0f884e3852651271315788a
Damian Brzoskowski - s18499 <s18499@pjwstk.edu.pl>
2021-06-07 20:49:23 +0200

    Base routing and product moodel - interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule} from "@angular/router"
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
-import {ProductListComponent} from "./product-list/product-list.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ProductListComponent
+    ProductListComponent,
+    NavigationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'product', component: ProductComponent},
+      {path: 'product-list', component: ProductListComponent},
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: '**', redirectTo: '/', pathMatch: 'full'}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
