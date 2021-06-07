f2575a05f1e00f99cbbd5c34572029ce07cd859a
Damian Brzoskowski - s18499 <s18499@pjwstk.edu.pl>
2021-06-07 20:10:43 +0200

    Base header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ItemComponent } from './product/item.component';
-import { ItemListComponent } from './item-list/item-list.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { ProductComponent } from './product/product.component';
+import {ProductListComponent} from "./product-list/product-list.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemComponent,
-    ItemListComponent,
-    ProductListComponent,
-    ProductComponent
+    ProductComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
